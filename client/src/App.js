574d34b6bdb598d294be85242b204821a6915248
Your Name <youremail@gmail.com>
2026-03-20 23:45:55 -0700

    fixed home layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SubjectList from './pages/SubjectList';
 import Programs from './pages/Programs';
